a135f452c7f28ef5745dd93eb57423683afa53b2
Semi Invader <=>
2023-02-27 17:11:31 -0600

    added docgen

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import "@nomicfoundation/hardhat-toolbox";
 import 'hardhat-contract-sizer';
+import 'solidity-docgen';
 
 // The next line is part of the sample project, you don't need it in your
 // project. It imports a Hardhat task definition, that can be used for
